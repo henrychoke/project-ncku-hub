9a07ddde0ec35acc688c4e8784b3fc3c155fd4e9
henrychoke <49486958+henrychoke@users.noreply.github.com>
2019-05-01 22:14:39 +0800

    Add files via upload

---
+++ template.php
@@ -0,0 +1,97 @@
+<html>
+<head>
+    <title>TEMPLATE</title>
+</head>
+<style>
+    .logo-wrap {
+    position: relative;
+    left: 0vw;
+    top: 0vw;
+    padding: 0px 15px;
+    }
+    .black-background{
+        background-color: #000000;
+    }
+    .w-inline-block {
+    max-width: 100%;
+    }
+    .w-list-unstyled {
+    padding-left: 0;
+    list-style: none;
+    }
+    .nav_items{
+    display: flex;
+    height: 100%;
+    margin-top: 0px;
+    margin-bottom: 0px;
+    padding-left: 0px;
+    }
+    .nav_item {
+    padding-right: 15px;
+    padding-left: 15px;
+    font-size: 13px;
+    }
+    .e-hero_top {
+    top: 20vh;
+    padding-bottom: 20px;
+    }
+    .headline {
+    margin-top: 0px;
+    margin-bottom: 20px;
+    margin-left: -3px;
+    color: #000000;
+    font-size: 60px;
+    line-height: 110%;
+    }
+    .an-empty-div-because-there-is-no-such-thing-as-min-padding {
+    height: 15vh;
+    min-height: 120px;
+    }
+    .nav-list{
+        display: flex;
+    }
+    .nav-inner{
+        position: absolute;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    overflow: hidden;
+    height: 60px;
+    flex: 0 0 auto;
+    background-color: #000;
+    }
+    .nav_main {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    z-index: 1;
+    display: flex;
+    height: 60px;
+    align-items: stretch;
+    background-color: rgba(0, 0, 0, .15);
+}
+
+</style>
+<body>
+<div class='nav_position'>
+    <div class='nav_inner' style='width: 1100px; height: 60px; background-color: rgb(0,0,0);'>
+            <h2 class='logo-wrap' style='color: white;'>webflow</h2>
+        <ul class='nav_item w-list-unstyled'>
+            <li class='nav_item' style='opacity: 1; display: inline'>
+                <h2 class='logo-wrap' style='color: white;'>features
+                </h2>
+            </li>
+        </ul>
+</div>
+<div class="an-empty-div-because-there-is-no-such-thing-as-min-padding"></div>
+<div class='e-hero_top'>
+    <h1 class='headline'>
+        'Your brand is unique.'<br>
+        'Your store should be too.'
+    </h1>
+</html>
